878e0cca9883cfc7fd11f9fa67d9a0ad930b044e
matias torres <matias.torres1812@gmail.com>
2023-11-20 12:33:50 -0300

    .

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import Navbar from './components/Navbar';  // Asegúrate de que la ruta al componente Navbar es correcta
+import HomeScreen from './src/screens/HomeScreen';
+// import Navbar from './components/Navbar';  
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Inicio" component={HomeScreen}/>
       </Stack.Navigator>
